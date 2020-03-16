c3fcf06c4961ebda280238c805114a144f09e1b7
MorganaGalamba <morgana.feijo@gmail.com>
2020-03-16 09:11:04 -0300

    oi

---
+++ Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  agua
+//
+//  Created by Morgana Beatriz Feijo Galamba on 10/03/20.
+//  Copyright © 2020 Morgana Beatriz Feijo Galamba. All rights reserved.
+//
+
+import Foundation
---
+++ agua/Person.swift
@@ -0,0 +1,61 @@
+//
+//  Person.swift
+//  agua
+//
+//  Created by Morgana Beatriz Feijo Galamba on 10/03/20.
+//  Copyright © 2020 Morgana Beatriz Feijo Galamba. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+        
+        var weight: Int
+        var waterDrinked: Float
+        var water: Float
+        var percentage: Float
+        
+        init(kg: Int,qtdAgua: Float,agua: Float){
+            self.percentage = 0
+            self.waterDrinked = qtdAgua
+            self.water = agua
+            self.weight = kg
+        }
+        
+        func drinked(amount:Float){
+            if(amount==0.03){
+                self.waterDrinked = self.waterDrinked + 0.030
+            }
+            else if(amount==0.2){
+                self.waterDrinked = self.waterDrinked + 0.2
+            }
+            else{
+                self.waterDrinked = self.waterDrinked + 0.5
+            }
+            
+            if (self.waterDrinked>self.water){
+                self.waterDrinked = self.water
+            }
+            
+            self.percentage = (self.waterDrinked*100)/self.water
+        }
+        
+        func notDrinked(amount:Float){
+            if(amount==0.03){
+                self.waterDrinked = self.waterDrinked - 0.030
+            }
+            else if(amount==0.2){
+                self.waterDrinked = self.waterDrinked - 0.2
+            }
+            else{
+                self.waterDrinked = self.waterDrinked - 0.5
+            }
+            if (self.waterDrinked<0){
+                self.waterDrinked = 0
+            }
+            self.percentage = (self.waterDrinked*100)/self.water
+        }
+    
+    
+}
+
